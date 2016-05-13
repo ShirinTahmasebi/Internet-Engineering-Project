a4b7d929361a7ab71cc313c103ca711c7ca3e806
Shirin Tahmasebi <tahmasebi@arsh.co>
2016-05-14 03:17:56 +0430

    Added Database :)

---
+++ Source Code/FakePonisha/target/generated-sources/annotations/ir/ac/sbu/fakeponisha/model/User_.java
@@ -0,0 +1,16 @@
+package ir.ac.sbu.fakeponisha.model;
+
+import ir.ac.sbu.fakeponisha.model.Resume;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-05-14T03:11:07")
+@StaticMetamodel(User.class)
+public class User_ { 
+
+    public static volatile SingularAttribute<User, Resume> resumeId;
+    public static volatile SingularAttribute<User, Integer> userId;
+    public static volatile SingularAttribute<User, String> username;
+
+}
